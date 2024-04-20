8c5ccdfd622be62b4017a83b2dcff5a36d16ce4f
Wanginirl <xinwanginirl@gmail.com>
2024-04-20 23:34:21 +0100

    add gui

---
+++ src/main/java/SimplerFormApp.java
@@ -0,0 +1,25 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.util.Objects;
+
+public class SimplerFormApp extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent parent = FXMLLoader.load(Objects.requireNonNull(this.getClass().getResource("/com/ncirl/SimplerForm.fxml")));
+        Scene scene = new Scene(parent, 800, 600);
+        primaryStage.setTitle("Smart Garden");
+        primaryStage.centerOnScreen();
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
